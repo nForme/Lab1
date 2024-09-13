cefee85b650cad84430ca2dd01c57a49c6645c11
DANICIMO <DANICIMO@HOME-PC>
2024-09-13 16:39:34 +0300

    Add project files.

---
+++ Lab1/Date.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab1
+{
+    public class Date
+    {
+        private int _year;
+        private int _month;
+        private int _day;
+
+        public Date(int year, int month, int day)
+        {
+            Year = year;
+            Month = month;
+            Day = day;
+        }
+        
+        public int Year 
+        { get { return _year; } set { _year = value; } }
+
+        public int Month
+        { get { return _month; } set { _month = value; } }
+        
+        public int Day
+        { get { return _day; } set { _day = value; } }
+
+        public void PrintDate() => Console.WriteLine($"{Day:00}/{Month:00}/{Year:0000}");
+
+        public int CalculateDaysLeftInYear(Date date)
+        {
+            DateTime currentDate = new DateTime(date.Year, date.Month, date.Day);
+            DateTime endOfYear = new DateTime(date.Year, 12, 31);
+
+            int daysLeftInYear = (int)(endOfYear - currentDate).TotalDays;
+
+            return daysLeftInYear;
+        }
+
+        public TimeSpan Subtract(Date other)
+        {
+            DateTime date1 = new DateTime(Year,  Month, Day);
+            DateTime date2 = new DateTime(other.Year, other.Month, other.Day);
+
+            return date2 - date1;
+        }
+    }
+}
---
+++ Lab1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+using Lab1;
+
+class Program
+{
+    static void Main(String[] args)
+    {
+        //First method
+        Console.WriteLine("First method");
+        Date date = new Date(2022, 11, 10);
+        date.PrintDate();
+        //Second method
+        Console.WriteLine("Second method");
+        int daysLeftInYear = date.CalculateDaysLeftInYear(date);
+        Console.WriteLine(daysLeftInYear);
+        //Tasks
+        Console.WriteLine("Tasks");
+        List<Date> dates = new List<Date>();
+
+        Date date1 = new Date(2001, 5, 1);
+        Date date2 = new Date(2001, 2, 5);
+        Date date3 = new Date(2002, 7, 13);
+        dates.Add(date1);
+        dates.Add(date2);
+        dates.Add(date3);
+        foreach (Date d in dates)
+        {
+            d.PrintDate();
+        }
+
+        TimeSpan timeDifference = date2.Subtract(date1);
+        int daysDifference = (int)timeDifference.TotalDays;
+        Console.WriteLine(daysDifference);   
+    }
+}
